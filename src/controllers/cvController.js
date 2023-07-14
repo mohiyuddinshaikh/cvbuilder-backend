6ed669cd3934d5215ff90665a402fc29bc0ca239
mohiyuddin <mohiyuddin.shaikh@neosoftmail.com>
2023-07-14 12:15:22 +0530

    delete cv and delete and create new cv functionality addedd

--- src/controllers/cvController.js
+++ src/controllers/cvController.js
@@ -1,25 +1,65 @@
 const CV = require("../models/CV");
 
 exports.addCV = async (req, res) => {
-    const { userId } = req.body;
-    // {
-    //     "userId": "64ad780bce5a19ea34002bff"
-    // }
+  const { userId } = req.body;
+  // {
+  //     "userId": "64ad780bce5a19ea34002bff"
+  // }
 
-    try {
-      const existingCV = await CV.findOne({ userId, isCompleted: false });
-  
-      if (existingCV) {
-        return res.status(400).json({ message: "You already have an existing CV." });
-      } else {
-        const newCV = new CV({ userId });
-        const savedCV = await newCV.save();
-  
-        return res.status(201).json({ cvId: savedCV._id });
-      }
-    } catch (err) {
-      return res.status(500).json({ message: `An error occurred while creating the CV: ${err}` });
+  try {
+    const existingCV = await CV.findOne({ userId, isCompleted: false });
+
+    if (existingCV) {
+      return res
+        .status(400)
+        .json({ message: "You already have an existing CV." });
+    } else {
+      const newCV = new CV({ userId });
+      const savedCV = await newCV.save();
+
+      return res.status(201).json({ cvId: savedCV._id });
     }
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ message: `An error occurred while creating the CV: ${err}` });
+  }
 };
 
+exports.deleteCV = async (req, res) => {
+  const { cvId } = req.params;
 
+  try {
+    await CV.findByIdAndDelete(cvId);
+
+    return res.status(200).json({ message: "CV deleted successfully." });
+  } catch (err) {
+    return res.status(500).json({
+      message: "An error occurred while deleting the CV.",
+      error: err,
+    });
+  }
+};
+
+exports.createNewCV = async (req, res) => {
+  const { cvId } = req.params;
+  const { userId } = req.body;
+  // http://localhost:3000/cvs/64b0edb74b2bf37dd5baceaa/delete-and-create-new
+  //   {
+  //     "userId": "64ad780bce5a19ea34002bff"
+  // }
+
+  try {
+    await CV.findByIdAndDelete(cvId);
+
+    const newCV = new CV({ userId });
+    const savedCV = await newCV.save();
+
+    return res.status(201).json({ cvId: savedCV._id });
+  } catch (err) {
+    // Handle the error
+    return res
+      .status(500)
+      .json({ message: "An error occurred while creating the new CV." });
+  }
+};
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const { addUser, fetchAllCVByUser } = require("../controllers/userController");
-const { addCV } = require("../controllers/cvController");
+const { addCV, deleteCV, createNewCV } = require("../controllers/cvController");
 
 router.use(express.json());
 
 // Add user
-router.post('/user/add-user', addUser);
-router.get("/users/:userId/cvs", fetchAllCVByUser)
+router.post("/user/add-user", addUser);
+router.get("/users/:userId/cvs", fetchAllCVByUser);
 
 // cv routes
-router.post('/cv/add-cv', addCV);
-
+router.post("/cv/add-cv", addCV);
+router.delete("/cv/:cvId", deleteCV);
+router.post("/cvs/:cvId/delete-and-create-new", createNewCV);
 
 module.exports = router;
